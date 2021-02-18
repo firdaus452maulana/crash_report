2a88fac867a818851773e1c815d9f55f081b6146
atharianr <atharianrahmadani@gmail.com>
2021-02-18 23:10:44 +0700

    dari rian <3

--- lib/tampilan/loginPage.dart
+++ lib/tampilan/loginPage.dart
@@ -2,13 +2,54 @@ import 'package:crash_report/tampilan/homePage.dart';
 import 'package:crash_report/tampilan/registerPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import '../models/authentication.dart';
+import 'package:provider/provider.dart';
 
-class loginPage extends StatelessWidget {
-  String _email, _password;
+class loginPage extends StatefulWidget {
+  @override
+  _loginPageState createState() => _loginPageState();
+}
 
+class _loginPageState extends State<loginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
+  // SUBMIT
+  Future<void> _submit() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+
+    try {
+      await Provider.of<Authentication>(context, listen: false)
+          .signIn(_authData['email'], _authData['password']);
+    } catch (error) {
+      var errorMessage = 'Email atau password salah!';
+      _showErrorDialog(errorMessage);
+    }
+  }
+
+  // ERROR DIALOGBOX
+  void _showErrorDialog(String msg) {
+    showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              title: Text(
+                "Something went wrong.",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              content: Text(msg, style: TextStyle(fontSize: 12)),
+            ));
+  }
+
+  // TAMPILAN
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,12 +110,17 @@ class loginPage extends StatelessWidget {
 
                                     // EMAIL
                                     Container(
-                                      height: 40,
                                       child: TextFormField(
+                                        cursorColor: Colors.black,
                                         style: TextStyle(fontSize: 12),
+                                        keyboardType:
+                                            TextInputType.emailAddress,
                                         decoration: new InputDecoration(
                                             fillColor: Colors.white,
-                                            border: InputBorder.none,
+                                            border: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(30)),
+                                            ),
                                             enabledBorder: OutlineInputBorder(
                                                 borderRadius: BorderRadius.all(
                                                     Radius.circular(30)),
@@ -86,14 +132,20 @@ class loginPage extends StatelessWidget {
                                                     Radius.circular(30)),
                                                 borderSide: BorderSide(
                                                     color: Color(0xFF031F4B))),
-                                            filled: true,
+                                            filled: false,
                                             contentPadding: EdgeInsets.only(
                                                 left: 24.0, right: 24.0),
                                             hintStyle: TextStyle(
                                                 fontSize: 12,
                                                 color: Color(0xFF000000)
                                                     .withOpacity(0.15)),
-                                            hintText: "email"),
+                                            hintText: "email",
+                                            errorBorder: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.all(Radius.circular(30)),
+                                                borderSide: BorderSide(color: Colors.red)),
+                                            focusedErrorBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30)), borderSide: BorderSide(color: Colors.red, width: 1)),
+                                            errorStyle: TextStyle(fontSize: 12)),
                                         obscureText: false,
                                         validator: (value) {
                                           if (value.isEmpty) {
@@ -101,7 +153,9 @@ class loginPage extends StatelessWidget {
                                           }
                                           return null;
                                         },
-                                        onSaved: (value) => _password = value,
+                                        onSaved: (value) {
+                                          _authData['email'] = value;
+                                        },
                                       ),
                                     ),
 
@@ -109,31 +163,52 @@ class loginPage extends StatelessWidget {
 
                                     // PASSWORD
                                     Container(
-                                      height: 40,
                                       child: TextFormField(
+                                        cursorColor: Colors.black,
                                         style: TextStyle(fontSize: 12),
                                         decoration: new InputDecoration(
-                                            fillColor: Colors.transparent,
-                                            border: InputBorder.none,
-                                            enabledBorder: OutlineInputBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(30)),
-                                                borderSide: BorderSide(
-                                                    color: Color(0xFF000000)
-                                                        .withOpacity(0.15))),
-                                            focusedBorder: OutlineInputBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(30)),
-                                                borderSide: BorderSide(
-                                                    color: Color(0xFF031F4B))),
-                                            filled: true,
-                                            contentPadding: EdgeInsets.only(
-                                                left: 24.0, right: 24.0),
-                                            hintStyle: TextStyle(
-                                                fontSize: 12,
-                                                color: Color(0xFF000000)
-                                                    .withOpacity(0.15)),
-                                            hintText: "password"),
+                                          fillColor: Colors.transparent,
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(30),
+                                            ),
+                                          ),
+                                          enabledBorder: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(30)),
+                                              borderSide: BorderSide(
+                                                  color: Color(0xFF000000)
+                                                      .withOpacity(0.15))),
+                                          focusedBorder: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(30)),
+                                              borderSide: BorderSide(
+                                                  color: Color(0xFF031F4B))),
+                                          filled: true,
+                                          contentPadding: EdgeInsets.only(
+                                              left: 24.0, right: 24.0),
+                                          hintStyle: TextStyle(
+                                              fontSize: 12,
+                                              color: Color(0xFF000000)
+                                                  .withOpacity(0.15)),
+                                          hintText: "password",
+                                          errorBorder: OutlineInputBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(30)),
+                                              borderSide: BorderSide(
+                                                  color: Colors.red)),
+                                          focusedErrorBorder:
+                                              OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(30)),
+                                                  borderSide: BorderSide(
+                                                      color: Colors.red,
+                                                      width: 1)),
+                                          errorStyle: TextStyle(
+                                            fontSize: 12,
+                                          ),
+                                        ),
                                         obscureText: true,
                                         validator: (value) {
                                           if (value.isEmpty) {
@@ -141,7 +216,9 @@ class loginPage extends StatelessWidget {
                                           }
                                           return null;
                                         },
-                                        onSaved: (value) => _password = value,
+                                        onSaved: (value) {
+                                          _authData['password'] = value;
+                                        },
                                       ),
                                     ),
 
@@ -163,7 +240,7 @@ class loginPage extends StatelessWidget {
                                               BorderRadius.circular(30)),
                                       textColor: Colors.white,
                                       child: Container(
-                                        height: 40,
+                                        height: 48,
                                         alignment: Alignment.center,
                                         child: Text(
                                           "Sign In",
@@ -172,47 +249,45 @@ class loginPage extends StatelessWidget {
                                               fontSize: 12),
                                         ),
                                       ),
-                                      onPressed: () {},
+                                      onPressed: () {
+                                        _submit();
+                                      },
+                                    ),
+
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Don't have an account?",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: Color(0xFF000000)
+                                                  .withOpacity(.25)),
+                                        ),
+                                        Container(
+                                          width: 54,
+                                          child: FlatButton(
+                                            padding: EdgeInsets.all(0.0),
+                                            child: Text(
+                                              "Sign Up",
+                                              style: TextStyle(
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            onPressed: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          registerPage()));
+                                            },
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Container(
-                              margin: EdgeInsets.only(bottom: 16),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Don't have an account?",
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        color:
-                                            Color(0xFF000000).withOpacity(.25)),
-                                  ),
-                                  Container(
-                                    width: 54,
-                                    child: FlatButton(
-                                      padding: EdgeInsets.all(0.0),
-                                      child: Text(
-                                        "Sign Up",
-                                        style: TextStyle(
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    registerPage()));
-                                      },
-                                    ),
-                                  ),
-                                ],
                               ),
                             ),
                           ),
@@ -226,22 +301,4 @@ class loginPage extends StatelessWidget {
           ],
         ));
   }
-
-  void _submit(){
-
-  }
-
-/*Future<void> _submit() async {
-    final formState = _formKey.currentState;
-    if(formState.validate()){
-      formState.save();
-      try{
-        AuthResult result =  await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
-        FirebaseUser user = result.user;
-        Navigator.push(context, MaterialPageRoute(builder: (context) => homePage()));
-      }catch(e){
-        print(e.message);
-      }
-    }
-  }*/
 }
