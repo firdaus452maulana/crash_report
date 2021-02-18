f5868c75250917d030c9eaaed9d26302f84fa9c0
Muhammad Firdaus Maulana <firdaus452maulana@gmail.com>
2021-02-18 21:34:54 +0700

    dari firdaus <main menu>

--- lib/mainMenuUser.dart
+++ lib/mainMenuUser.dart
@@ -1,5 +1,42 @@
+import 'package:flutter/material.dart';
+
+enum DialogAction {save, close}
+
 class addBarang{
   String namaAlat;
   String lokasi;
   String divisi;
+}
+
+class mainMenuUser extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return showDialog(
+              context: context,
+            builder: (context) {
+                return AlertDialog(
+                  content: Column (
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    //posisi
+                    mainAxisSize: MainAxisSize.min,
+                    // untuk mengatur agar widget column mengikuti widget
+                    children: <Widget>[
+                      TextFormField
+                    ],
+                  ),
+                )
+            }
+          );
+        },
+      ),
+    );
+  }
 }
