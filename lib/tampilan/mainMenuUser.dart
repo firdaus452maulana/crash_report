deb5b092680f43c61ef1e8ff4dbbcfdac493fc41
Muhammad Firdaus Maulana <firdaus452maulana@gmail.com>
2021-02-25 14:06:12 +0700

    dari firdaus ヾ(≧▽≦*)o : Adding ListView

--- lib/tampilan/mainMenuUser.dart
+++ lib/tampilan/mainMenuUser.dart
@@ -1,7 +1,7 @@
 import 'package:crash_report/tampilan/loginPage.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,6 +17,7 @@ class _mainMenuUserState extends State<mainMenuUser> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   TextEditingController _namaAlatController, _lokasiController, _divisiController;
 
+  Query _query;
   DatabaseReference _ref;
   @override
   void initState() {
@@ -25,6 +26,7 @@ class _mainMenuUserState extends State<mainMenuUser> {
     _lokasiController = TextEditingController();
     _divisiController = TextEditingController();
     _ref = FirebaseDatabase.instance.reference().child('listBarang');
+    _query = FirebaseDatabase.instance.reference().child('listBarang').orderByChild('nama');
   }
 
   void _showDialogPenambahan(){
@@ -271,6 +273,96 @@ class _mainMenuUserState extends State<mainMenuUser> {
         });
   }
 
+  Widget _buildListBarang({Map barang}){
+
+    return Container(
+      //height: 150,
+      color: Colors.white,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.05),
+              spreadRadius: 10,
+              blurRadius: 10,
+              offset: Offset(0, 0),
+            )
+          ],
+          borderRadius: BorderRadius.circular(17.5),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 12.0, bottom: 12.0, left: 24.0, right: 24.0),
+          child: Stack(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                //posisi
+                mainAxisSize: MainAxisSize.min,
+                // untuk mengatur agar widget column mengikuti widget
+                children: <Widget>[
+                  Text(barang['nama'],
+                    style: GoogleFonts.openSans(
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(barang['letak'],
+                    style: GoogleFonts.openSans(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 12,
+                        color: Colors.black.withOpacity(0.25)
+                    ),
+                  ),
+                  Text(barang['divisi'],
+                    style: GoogleFonts.openSans(
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  //posisi
+                  mainAxisSize: MainAxisSize.min,
+                  // untuk mengatur agar widget column mengikuti widget
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text("Status",
+                        style: GoogleFonts.openSans(
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(barang['status'],
+                        style: GoogleFonts.openSans(
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   Future<void> _signOut(BuildContext context) async {
     await _auth.signOut().then((value) {
       Navigator.push(context, MaterialPageRoute(builder: (context) => loginPage()));
@@ -289,7 +381,12 @@ class _mainMenuUserState extends State<mainMenuUser> {
               preference.remove('email');
               _signOut(context);
             },
-          )
+          ),
+          FirebaseAnimatedList(query: _query,itemBuilder: (BuildContext context,
+              DataSnapshot snapshot, Animation<double>animation, int index){
+            Map barang = snapshot.value;
+            return _buildListBarang(barang: barang);
+          },),
         ],
       ),
 
