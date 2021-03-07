6950682188e8cc6f55b24d70ef3b467ea225825f
atharianr <atharianrahmadani@gmail.com>
2021-03-08 00:15:19 +0700

    dari rian <3

--- lib/tampilan/loadingPage.dart
+++ lib/tampilan/loadingPage.dart
@@ -1,4 +1,4 @@
-import 'package:crash_report/listbarangPage_teknisi.dart';
+import 'file:///D:/flutter/crash_report/lib/tampilan/listbarangPage_teknisi.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
