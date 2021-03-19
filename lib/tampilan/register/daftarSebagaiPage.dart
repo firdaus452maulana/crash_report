09f017dc619be38bcf231d9e02070fdd5d8b076a
atharianr <atharianrahmadani@gmail.com>
2021-03-19 22:57:23 +0700

    dari rian <3

--- lib/tampilan/register/daftarSebagaiPage.dart
+++ lib/tampilan/register/daftarSebagaiPage.dart
@@ -13,7 +13,7 @@ class daftarSebagai extends StatefulWidget {
 class _daftarSebagaiState extends State<daftarSebagai> {
   List<String> lst = ['Pegawai', 'Admin', 'Teknisi'];
   int selectedIndex = 0;
-  int indexnavigate =0;
+  int indexnavigate = 0;
   String textUser = 'Pegawai';
   String textBaca = 'pe.ga.wai';
   String textFitur = 'fitur';
@@ -30,113 +30,91 @@ class _daftarSebagaiState extends State<daftarSebagai> {
                   image: AssetImage('assets/background.png'),
                   fit: BoxFit.fill)),
         ),
-        Container(
-          margin: EdgeInsets.only(left: 32, top: 100, right: 32, bottom: 100),
-          width: double.infinity,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(17.5)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 40, left: 40),
-                //color:Colors.red,
-                child: Text(
-                  "Daftar sebagai :",
-                  textAlign: TextAlign.left,
-                  style: GoogleFonts.openSans(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 40, top: 10),
-                child: Column(
-                  children: <Widget>[
-                    customRadio(lst[0], 0),
-                    customRadio(lst[1], 1),
-                    customRadio(lst[2], 2),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 45, top: 20),
-                child: Text(
-                  '$textUser',
-                  style: GoogleFonts.openSans(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 45),
-                child: Text(
-                  '$textBaca',
-                  style: GoogleFonts.openSans(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black.withOpacity(0.5)),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 45),
-                child: Text(
-                  '$textFitur',
-                  style: GoogleFonts.openSans(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black.withOpacity(0.5)),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 45),
-                child: Text(
-                  '$textIsiFitur',
-                  style: GoogleFonts.openSans(
-                      fontSize: 10,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black.withOpacity(0.5)),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 30, left: 40),
-                child: Row(
-                  children: [
-                    RaisedButton(
-                      onPressed: (){
-                        Navigator.of(context).pop();
-                      },
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(17.5)),
-                      padding: EdgeInsets.all(0),
-                      child: Container(
-                        height: 40,
-                        width: 105,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(17.5),
-                          color: Color(0xFFC3CEDA),
-                        ),
-                        child: Container(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Kembali',
-                            style: GoogleFonts.openSans(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black.withOpacity(0.2)),
-                          ),
-                        ),
-                      ),
+        Center(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.only(left: 32, right: 32),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(17.5)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 40, left: 40, right: 40),
+                    //color:Colors.red,
+                    child: Text(
+                      "Daftar sebagai :",
+                      textAlign: TextAlign.left,
+                      style: GoogleFonts.openSans(
+                          fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(
-                      width: 10,
+                  ),
+                  Container(
+                    //color: Colors.red,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        customRadio(lst[0], 0),
+                        customRadio(lst[1], 1),
+                        customRadio(lst[2], 2),
+                      ],
                     ),
-                    ButtonMulai(indexnavigate),
-                  ],
-                ),
+                  ),
+                  Container(
+                    //color: Colors.red,
+                    margin: EdgeInsets.only(left: 45, top: 20, right: 45),
+                    child: Text(
+                      '$textUser',
+                      style: GoogleFonts.openSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 45, right: 45),
+                    child: Text(
+                      '$textBaca',
+                      style: GoogleFonts.openSans(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black.withOpacity(0.5)),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 45, right: 45),
+                    child: Text(
+                      '$textFitur',
+                      style: GoogleFonts.openSans(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black.withOpacity(0.5)),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 45, right: 45),
+                    child: Text(
+                      '$textIsiFitur',
+                      style: GoogleFonts.openSans(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black.withOpacity(0.5)),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    margin: EdgeInsets.only(
+                        left: 45, right: 45, bottom: 40, top: 24),
+                    //color: Colors.red,
+                    child: ButtonMulai(indexnavigate),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ]),
@@ -155,10 +133,10 @@ class _daftarSebagaiState extends State<daftarSebagai> {
     });
   }
 
-  void GetIndex(int index){
+  void GetIndex(int index) {
     setState(() {
       selectedIndex = index;
-      indexnavigate=index;
+      indexnavigate = index;
     });
   }
 
@@ -212,23 +190,17 @@ class _daftarSebagaiState extends State<daftarSebagai> {
 
   Widget ButtonMulai(int nav) {
     return RaisedButton(
+      color: Color(0xFF031F4B),
       onPressed: () => NavigateUser(nav),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17.5)),
       padding: EdgeInsets.all(0),
       child: Container(
-        height: 40,
-        width: 105,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(17.5),
-          color: Color(0xFF031F4B),
-        ),
-        child: Container(
-          alignment: Alignment.center,
-          child: Text(
-            'Mulai',
-            style: GoogleFonts.openSans(
-                fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
+        width: double.infinity,
+        child: Text(
+          'Lanjut',
+          textAlign: TextAlign.center,
+          style: GoogleFonts.openSans(
+              fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
         ),
       ),
     );
@@ -236,29 +208,25 @@ class _daftarSebagaiState extends State<daftarSebagai> {
 
   Widget customRadio(String txt, int index) {
     return Container(
-      margin: EdgeInsets.only(top: 10),
+      margin: EdgeInsets.only(top: 10, left: 38, right: 38),
       child: RaisedButton(
         onPressed: () => changeAll(index),
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(17.5)),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         padding: EdgeInsets.all(0),
+        color: selectedIndex == index ? Color(0xFF031F4B) : Color(0xFFC3CEDA),
         child: Container(
           height: 60,
-          width: 215,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(17.5),
-            color:
-                selectedIndex == index ? Color(0xFF031F4B) : Color(0xFF738FA7),
-          ),
-          child: Container(
-            margin: EdgeInsets.only(top: 12, left: 20),
-            child: Text(
-              txt,
-              style: GoogleFonts.openSans(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: selectedIndex == index ? Colors.white : Colors.white),
-            ),
+          width: double.infinity,
+          margin: EdgeInsets.only(left: 20, right: 20),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            txt,
+            style: GoogleFonts.openSans(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: selectedIndex == index
+                    ? Colors.white
+                    : Colors.black.withOpacity(0.2)),
           ),
         ),
       ),
